ba436edf073ed9b4825eae633427f5df0ea945b0
mlesk <lesk.mark.gmail.com>
2018-07-14 15:21:23 -0400

    Few more shapes

--- src/index.js
+++ src/index.js
@@ -13,6 +13,24 @@ const drawCircleOfCircles = (d, w, h) => {
 };
 
 //==============================================================================
+const drawSmallCircleOfCircles = (d, w, h) => {
+  d.noStroke();
+  for (var i = 0; i < 10; i++) {
+    d.ellipse(0, w / 4.5, w / 4.5, w / 4.5);
+    d.rotate(Math.PI / 5);
+  }
+};
+
+//==============================================================================
+const dots = (d, w, h) => {
+  d.noStroke();
+  for (var i = 0; i < 10; i++) {
+    d.ellipse(0, w / 4.5, w / 4.5, w / 4.5);
+    d.rotate(Math.PI / 1);
+  }
+};
+
+//==============================================================================
 const drawFlower = (d, w, h) => {
   const drawOutline = false;
   const drawFill = true;
@@ -33,6 +51,6 @@ const drawFlower = (d, w, h) => {
 rotatingShape({
   grid: 8,
   randomRotation: false,
-  drawShape: drawCircleOfCircles,
+  drawShape: drawSmallCircleOfCircles,
   suppressRotation: false,
 });
